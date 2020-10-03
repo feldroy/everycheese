d64c8a793762cbaa517eb58cc0ce83fb46738c33
Daniel Feldroy <daniel@feldroy.com>
2020-10-03 15:20:39 -0700

    Add cheeses app

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,6 +86,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "everycheese.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "everycheese.cheeses.apps.CheesesConfig"
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ everycheese/cheeses/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+
+class Cheese(TimeStampedModel):
+
+    class Firmness(models.TextChoices):
+        UNSPECIFIED = "unspecified", "Unspecified"
+        SOFT = "soft", "Soft"
+        SEMI_SOFT = "semi-soft", "Semi-Soft"
+        SEMI_HARD = "semi-hard", "Semi-Hard"
+        HARD = "hard", "Hard"
+
+
+    name = models.CharField("Name of Cheese", max_length=255)
+    slug = AutoSlugField(
+        "Cheese address",
+        unique=True,
+        always_update=False,
+        populate_from="name")
+
+    description = models.TextField("Description", blank=True)
+
+    firmness = models.CharField("Firmness", max_length=20,
+        choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+
+
+    def __str__(self):
+        return self.name
